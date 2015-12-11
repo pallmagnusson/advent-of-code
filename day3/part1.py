94315a55b981e4c362143b290f30ab63755ac9d0
Pall Magnusson <pall@bullionbypost.co.uk>
2015-12-11 17:24:32 +0000

    Baby steps

---
+++ day3/part1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+NORTH = '^'
+SOUTH = 'v'
+EAST = '>'
+WEST = '<'
+
+with open('input', 'r') as f:
+    directions = list(f.read())[:-1]
+
+houses = 1
+for index, direction in enumerate(directions):
+    pass
+
+print houses
