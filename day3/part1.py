b56e174978e8ec318b265feea10c3cf71225aa18
Pall Magnusson <vargur@gmail.com>
2015-12-13 19:54:16 +0000

    Solve day 3 part 1

--- day3/part1.py
+++ day3/part1.py
@@ -8,8 +8,27 @@ WEST = '<'
 with open('input', 'r') as f:
     directions = list(f.read())[:-1]
 
+route = [[0, 0]]
 houses = 1
 for index, direction in enumerate(directions):
-    pass
+    last_x_pos = route[-1][0]
+    last_y_pos = route[-1][1]
+
+    if direction == NORTH:
+        new_pos = [last_x_pos, last_y_pos + 1]
+
+    if direction == SOUTH:
+        new_pos = [last_x_pos, last_y_pos - 1]
+
+    if direction == EAST:
+        new_pos = [last_x_pos + 1, last_y_pos]
+
+    if direction == WEST:
+        new_pos = [last_x_pos - 1, last_y_pos]
+
+    if new_pos not in route:
+        houses += 1
+
+    route.append(new_pos)
 
 print houses
