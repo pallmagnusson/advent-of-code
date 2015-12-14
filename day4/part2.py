dd2db9f5fa7c540b3197d50fd66c21bedbc49f72
Pall Magnusson <vargur@gmail.com>
2015-12-14 21:55:27 +0000

    Solve day 4

---
+++ day4/part2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import hashlib
+
+SECRET = 'iwrupvqb'
+
+count = 1
+while True:
+    md5hash = hashlib.md5('{}{}'.format(SECRET, count)).hexdigest()
+    if md5hash.startswith('000000'):
+        print count
+        break
+    count += 1
