2e7666fdb6c704b4a8ac3ea7732917c831707b5c
Pall Magnusson <pall@bullionbypost.co.uk>
2015-12-15 17:44:31 +0000

    Solve day 5 part 1

--- day5/part1.py
+++ day5/part1.py
@@ -3,29 +3,37 @@
 ALLOWED = ['a', 'e', 'i', 'o', 'u']
 DISALLOWED = ['ab', 'cd', 'pq', 'xy']
 
-nice_strings = 0
-with open('input', 'r') as f:
-    for line in f:
-        allowed_count = 0
-        for letter in ALLOWED:
-            if letter in line:
-                allowed_count += 1
 
-        has_double_letter = False
-        letters = list(line)[:-1]
-        for index, letter in enumerate(letters):
-            if index > 0:
-                if letter == letters[index - 1]:
-                    has_double_letter = True
-                    break
+def has_allowed_count(line):
+    allowed_count = 0
+    for letter in list(line):
+        if letter in ALLOWED:
+            allowed_count += 1
+    if allowed_count > 2:
+        return True
+    return False
 
-        contains_disallowed = False
-        for item in DISALLOWED:
-            if item in line:
-                contains_disallowed = True
-                break
 
-        if allowed_count > 2 and has_double_letter and not contains_disallowed:
-            nice_strings += 1
-            
-print nice_strings
+def has_double_letter(line):
+    letters = list(line)[:-1]
+    for index, letter in enumerate(letters):
+        if index > 0:
+            if letter == letters[index - 1]:
+                return True
+    return False
+
+
+def has_disallowed(line):
+    for item in DISALLOWED:
+        if item in line:
+            return True
+    return False
+
+
+if __name__ == "__main__":
+    nice_strings = 0
+    with open('input', 'r') as f:
+        for line in f:
+            if has_allowed_count(line) and has_double_letter(line) and not has_disallowed(line):
+                nice_strings += 1
+    print nice_strings
