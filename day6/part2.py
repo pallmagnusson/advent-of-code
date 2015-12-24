783bfa7c01cc7efe15f574f67e9e180406af0379
Pall Magnusson <vargur@gmail.com>
2015-12-24 16:05:05 +0100

    Solve day 6 part 2

--- day6/part2.py
+++ day6/part2.py
@@ -1,31 +1,36 @@
 #!/usr/bin/python
 
-LIGHTS = set()
+LIGHTS = {}
+
+
+def init_lights():
+    for x in range(1000):
+        for y in range(1000):
+            LIGHTS[(x, y)] = 0
 
 
 def turn_on(src_coord, dst_coord):
     for x in range(src_coord[0], dst_coord[0] + 1):
         for y in range(src_coord[1], dst_coord[1] + 1):
-            LIGHTS.add((x, y))
+            LIGHTS[(x, y)] += 1
 
 
 def turn_off(src_coord, dst_coord):
     for x in range(src_coord[0], dst_coord[0] + 1):
         for y in range(src_coord[1], dst_coord[1] + 1):
-            if (x, y) in LIGHTS:
-                LIGHTS.remove((x, y))
+            if LIGHTS[(x, y)] > 0:
+                LIGHTS[(x, y)] -= 1
             
 
 def toggle(src_coord, dst_coord):
     for x in range(src_coord[0], dst_coord[0] + 1):
         for y in range(src_coord[1], dst_coord[1] + 1):
-            if (x, y) in LIGHTS:
-                LIGHTS.remove((x, y))
-            else:
-                LIGHTS.add((x, y))
+            LIGHTS[(x, y)] += 2
 
 
 if __name__ == "__main__":
+    init_lights()
+
     with open('input', 'r') as f:
         for line in f:
             instructions = line.replace('\n', '').split(' ')
@@ -37,6 +42,7 @@ if __name__ == "__main__":
 
                 if instructions[1] == 'on':
                     turn_on(src_coord, dst_coord)
+
                 else:
                     turn_off(src_coord, dst_coord)
             elif action == 'toggle':
@@ -44,4 +50,4 @@ if __name__ == "__main__":
                 dst_coord = map(int, instructions[3].split(','))
                 toggle(src_coord, dst_coord)
 
-    print len(LIGHTS)
+    print sum(LIGHTS.values())
